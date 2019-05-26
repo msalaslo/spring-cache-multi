44d350da9322fe7762811269b2074bd78b30c729
Miguel Salas Lopez <miguel.salas@external.securitasdirect.es>
2019-05-26 12:25:49 +0200

    hazelcast included in the cachemanger

--- src/main/java/com/msl/cache/springcachemulti/config/HazelcastCacheConfig.java
+++ src/main/java/com/msl/cache/springcachemulti/config/HazelcastCacheConfig.java
@@ -1,10 +1,6 @@
 package com.msl.cache.springcachemulti.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,24 +8,27 @@ import org.springframework.context.annotation.Profile;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
  
 @Configuration
 @Profile("hazelcast-cache")
 public class HazelcastCacheConfig {
-	
-//    @Bean
-//    @Profile("client")
-//    HazelcastInstance hazelcastInstance() {
-//        return HazelcastClient.newHazelcastClient();    // (2)
-//    }
 
-//    @Bean
-//    CacheManager cacheManager() {
-//        return new HazelcastCacheManager(hazelcastInstance()); // (3)
-//    }
-// 
+	@Bean
+    CacheManager cacheManager() {
+        return new HazelcastCacheManager(hazelcastInstance()); // (3)
+    }
+	
+    @Bean
+    @Profile("client")
+    HazelcastInstance hazelcastInstance() {
+        // for client HazelcastInstance LocalMapStatistics will not available
+//        return HazelcastClient.newHazelcastClient();
+        return Hazelcast.newHazelcastInstance();
+    }
+    
     @Bean
     public Config hazelCastConfig() {
  
@@ -63,19 +62,5 @@ public class HazelcastCacheConfig {
  
         return config;
     }
-    
-	/**
-	 * Caffeine cache manager.
-	 *
-	 * @return the cache manager
-	 */
-	@Bean(name = "hazelcastCacheManager")
-	public CacheManager hazelcastCacheManager() {
-		List<CaffeineCache> cacheList = new ArrayList<>();
-		HazelcastCacheManager cacheManager = new HazelcastCacheManager();
-
-//		cacheManager.setCaches(cacheList);
-		return cacheManager;
-	}
  
 }
